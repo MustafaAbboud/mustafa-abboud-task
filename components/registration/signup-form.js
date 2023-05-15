39a9f8f69a7579c39e62fc559814a19e70bb4dcf
MustafaAbboud <mustafaabbassabboud@gmail.com>
2023-05-15 14:33:29 +0300

    auth added (not done)

--- components/registration/signup-form.js
+++ components/registration/signup-form.js
@@ -1,36 +1,53 @@
 import { useRef, useState } from 'react';
+import { signIn } from 'next-auth/react';
 
 import classes from './signup-form.module.css';
 
 function SignupForm() {
 
+    const [isLogin, setIsLogin] = useState(true);
     const [isChecked, setIsChecked] = useState(false)
 
     const nameInputRef = useRef();
     const emailInputRef = useRef();
     const passwordInputRef = useRef();
-    const typeInputRef = useRef();
-
 
     async function submitHandler(event) {
         event.preventDefault();
 
-        const enteredName = nameInputRef.current.value;
         const enteredEmail = emailInputRef.current.value;
         const enteredPassword = passwordInputRef.current.value;
-        const admin = isChecked;
 
-        const user = {
-            name: enteredName,
-            email: enteredEmail,
-            password: enteredPassword,
-            admin: admin,
-        }
+        if (isLogin) {
+            const result = await signIn('credentials', {
+                redirect: false,
+                email: enteredEmail,
+                password: enteredPassword,
+            });
 
-        try {
-            const result = await createUser(user);
-        } catch (error) {
-            console.log(error);
+            if (!result.error) {
+                // set some auth state
+                // router.replace('/profile');
+                console.log(result)
+                alert('Login Successful')
+            }
+        } else {
+
+            const enteredName = nameInputRef.current.value;
+            const admin = isChecked;
+
+            const user = {
+                name: enteredName,
+                email: enteredEmail,
+                password: enteredPassword,
+                admin: admin,
+            }
+
+            try {
+                const result = await createUser(user);
+            } catch (error) {
+                console.log(error);
+            }
         }
     }
 
@@ -55,17 +72,19 @@ function SignupForm() {
 
     return (
         <section className={classes.auth}>
-            <h1>Login</h1>
+            <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
             <form onSubmit={submitHandler}>
-                <div className={classes.control}>
-                    <label htmlFor='name'>User Name</label>
-                    <input
-                        type='text'
-                        id='name'
-                        required
-                        ref={nameInputRef}
-                    />
-                </div>
+                {!isLogin &&
+                    <div className={classes.control}>
+                        <label htmlFor='name'>User Name</label>
+                        <input
+                            type='text'
+                            id='name'
+                            required
+                            ref={nameInputRef}
+                        />
+                    </div>
+                }
                 <div className={classes.control}>
                     <label htmlFor='email'>Your Email</label>
                     <input
@@ -84,17 +103,26 @@ function SignupForm() {
                         ref={passwordInputRef}
                     />
                 </div>
-                <div className={classes.control}>
-                    <label htmlFor='type'>Admin</label>
-                    <input
-                        type='checkbox'
-                        id='type'
-                        value={isChecked}
-                        onChange={() => setIsChecked(!isChecked)}
-                    />
-                </div>
+                {!isLogin &&
+                    <div className={classes.control}>
+                        <label htmlFor='type'>Admin</label>
+                        <input
+                            type='checkbox'
+                            id='type'
+                            value={isChecked}
+                            onChange={() => setIsChecked(!isChecked)}
+                        />
+                    </div>
+                }
                 <div className={classes.actions}>
-                    <button>Login</button>
+                    <button>{isLogin ? 'Login' : 'Sign Up'}</button>
+                    <button
+                        type='button'
+                        className={classes.toggle}
+                        onClick={() => setIsLogin(!isLogin)}
+                    >
+                        {isLogin ? 'Create new account' : 'Login with existing account'}
+                    </button>
                 </div>
             </form>
         </section>
